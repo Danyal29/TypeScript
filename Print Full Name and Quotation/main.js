8559ebf05b202a195b873ae572b323444fd3ae26
Sardar Muhammad Danyal Khan <150451875+Danyal29@users.noreply.github.com>
2024-04-23 14:01:21 +0500

    Add files via upload

---
+++ Print Full Name and Quotation/main.js
@@ -0,0 +1,12 @@
+"use strict";
+// Print Full Name and Quotation 
+let fname = 'Nelson';
+let mname = 'Rolihlahla';
+let lname = 'Mandela';
+console.log(`${fname} ${mname} ${lname}`);
+// Quotation 
+let Quotation = '"Education is the most powerful weapon which you can use to change the world." - Nelson Mandela';
+console.log(Quotation);
+let Quote = "Education is the most powerful weapon which you can use to change the world.";
+let author = "- Nelson Mandela";
+console.log(`${Quote} ${author}`);
--- Print Full Name and Quotation/main.ts
+++ Print Full Name and Quotation/main.ts
@@ -1,1 +1,16 @@
+// Print Full Name and Quotation 
 
+let fname:string = 'Nelson' 
+let mname:string = 'Rolihlahla'
+let lname:string = 'Mandela'
+
+console.log(`${fname} ${mname} ${lname}`) 
+
+// Quotation 
+
+let Quotation = '"Education is the most powerful weapon which you can use to change the world." - Nelson Mandela'
+console.log(Quotation)
+
+let Quote = "Education is the most powerful weapon which you can use to change the world."
+let author = "- Nelson Mandela"
+console.log(`${Quote} ${author}`)
