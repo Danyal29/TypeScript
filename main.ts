1e23b2d34134a75f118971e08d321335592b534c
Sardar Muhammad Danyal Khan <150451875+Danyal29@users.noreply.github.com>
2024-05-02 20:20:56 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,8 @@
+// purchase_statement_generator
+
+let qty:number = 9;
+let item:string = "laptop";
+let price:number = 300;
+
+let s1:string = ` I want ${qty} ${item} for ${price} $`;
+console.log(s1)
